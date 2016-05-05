5d9d794589a53eef29be8def593915856dd5308c
Mleon195 <mleon@mail.ccsf.edu>
2016-05-04 18:08:44 -0700

    hw part 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,10 +6,15 @@
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
-//
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require bootstrap.min
+//= require title
+//= require roses_are_red
+//= require violets_are_blue
+//= require compile_your_assets
+//= require then_hit_opt_cmd_u
+//= require poem_meta_data
